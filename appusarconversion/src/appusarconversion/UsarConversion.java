d2dc15a0be6891e016b56b988d9d4726620307cf
Mañanas <Maanas@SB09-10>
2023-11-30 09:50:33 +0100

    commit

---
+++ appusarconversion/src/appusarconversion/UsarConversion.java
@@ -0,0 +1,11 @@
+package appusarconversion;
+
+import appConversion.Conversion;
+
+public class UsarConversion {
+
+	public static void main(String[] args) {
+	
+	Conversion.convertCmPulgadas(0);
+	}
+}
